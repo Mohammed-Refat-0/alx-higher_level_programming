aaf860bdc6b1df7aefbd8f6e6bac4c6efd69b55b
Mohammed Refat <mohammedrefat53@outlook.com>
2023-10-31 20:26:09 +0200

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,6 +6,6 @@ def uppercase(str):
             list.append(ord(i) - 32)
         else:
             list.append(ord(i))
-    for i in range(len(list)):
+    for i in list:
         print("{}".format(chr(i)), end="")
-        print()
+    print()
