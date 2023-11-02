eca75694db1ef9c8959469fcc150407655c1581b
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-02 21:19:05 +0200

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,12 +6,10 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+    op = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(op.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, op[sys.argv[2]](a, b)))
