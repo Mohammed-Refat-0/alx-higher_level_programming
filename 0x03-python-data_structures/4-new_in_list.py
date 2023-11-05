7d83a5d35099a2583f1e0a5ca5da850800628396
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-05 17:12:58 +0200

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     list1 = my_list.copy()
     if len(my_list) <= idx or idx < 0:
         return list1
