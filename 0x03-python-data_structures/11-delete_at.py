821a52f66a363b2ff4c746ae472ae145f36e2fef
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-05 23:40:42 +0200

    task 11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if len(my_list) - 1 < idx or idx < 0:
+        return my_list
+    else:
+        my_list.remove(idx)
+        return my_list
