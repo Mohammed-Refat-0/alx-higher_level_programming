3208b3dc175b87730993e87cd54557ea30f8eb6f
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-05 23:27:31 +0200

    task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new = []
+    for i in range(len(new)):
+        if my_list[i] % 2 == 0:
+            new.append(True)
+        else:
+            new.append(False)
+    return new
