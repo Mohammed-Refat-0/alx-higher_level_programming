cd35be8bd97a9191b650ce908fa237d721d00cd5
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-05 23:22:55 +0200

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,6 +4,6 @@ def max_integer(my_list=[]):
         return None
     max = mylist[0]
     for i in range(len(my_list)):
-        if i > max:
-            max = i
+        if my_list[i] > max:
+            max = my_list[i]
     return max
