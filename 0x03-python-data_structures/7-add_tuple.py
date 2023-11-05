f5c58e128d10b2ee79aa6301b7c8c9c77f83a767
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-05 20:09:04 +0200

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    new = ()
-    value = 0
-    for i in 1:
-        if tuple_a(i):
-            value = value + tuple_a(i)
-        if tuple_b(i):
-            value = value + tuple_b(i)
-        new.append(value)
-        value = 0
-    return new
+    sum_tuple = ()
+    for i in range(2):
+        val_a = tuple_a[i] if i < len(tuple_a) else 0 + tuple_b[i] if i < len(tuple_b) else 0
+        val_b = tuple_b[i] if i < len(tuple_b) else 0 + tuple_a[i] if i < len(tuple_a) else 0
+        sum_tuple += (val_a, val_b)
+    return sum_tuple
