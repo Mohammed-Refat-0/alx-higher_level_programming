d4c4dadc251192d0aed7177c12a92a6c3e53f0cd
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-17 18:42:14 +0200

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 def square_matrix_simple(matrix=[]):
     newmatrix = matrix.copy()
     for i in range(len(newmatrix)):
-        newmatrix[i] = list(map(lambda x: x**2, newmatrix[i]))
+        newmatrix[i] = (map(lambda x: x**2, newmatrix[i])
     return (newmatrix)
