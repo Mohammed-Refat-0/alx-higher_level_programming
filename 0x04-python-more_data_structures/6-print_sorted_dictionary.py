a764a17ccb753c88a6f87ef0e537a2835ad3523c
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-17 22:16:46 +0200

    task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_dic = dic(sorted(a_dictionary.items()))
+    sorted_dic = dict(sorted(a_dictionary.items()))
     for i in sorted_dic:
         print(i)
