790fe7ac3a1a063180231e39340f88b121160a8b
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-17 21:39:09 +0200

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    newlist = my_list.copy()
-    for i in range(len(newlist)):
-        if newlist[i] == search:
-            newlist[i] = newlist[i].replace(replace)
-    return newlist
+    return [replace if i == search else  i for i in my_list]
