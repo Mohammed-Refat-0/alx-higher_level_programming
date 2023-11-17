c5d6800d35d24f5be30a8ea7ac3403396cd13d03
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-17 18:48:39 +0200

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     newmatrix = []
     for c in matrix:
         cnew = list(map(lambda x: x**2, c))
-        new_matrix.append(cnew)
+        newmatrix.append(cnew)
     return newmatrix
