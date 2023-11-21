676c4e395d83aa4f6feb97b8014167e295937255
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-21 02:20:08 +0200

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
             count = +1
             i = +1
         except IndexError:
-            print("out of index error")
+            break
         finally:
             print("")
             return count
