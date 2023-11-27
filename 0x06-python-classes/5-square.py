f7f59e21e82d65302d59522f7fd86455a12d1877
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-27 07:59:08 +0200

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """A Square class"""
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size (int, optional): size of square. Defaults to 0.
+
+        Raises:
+            ValueError: if value < 0
+            TypeError: if type not int
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        """calcalute square area
+        Returns:
+            _type_: int area of sqaure
+        """
+        return (self.__size) ** 2
+
+    def my_print(self):
+        """print sqarue
+        """
+        if self.size == 0:
+            print('')
+        else:
+            for i in range(self.size):
+                for j in range(self.size):
+                    print('#')
+                print('')
+
+    @property
+    def size(self):
+        """getter of __size
+        Returns:
+            The size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter of __size
+        Args:
+            value (int): the size of a size of the square
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
