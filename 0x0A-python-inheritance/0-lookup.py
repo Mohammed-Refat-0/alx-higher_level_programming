627ab327b3bca868d078717b108087692c0aa504
Mohammed Refat <mohammedrefat53@outlook.com>
2023-12-04 18:45:50 +0200

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+    """A function that returns all avaliable attributes of an object
+
+    Returns:
+        list: list o an objects's attributes
+    """
+def lookup(obj):
+    return dir(obj)
