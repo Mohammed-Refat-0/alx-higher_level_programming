b380a0423ff45c3adef51c84103ecd7af67576c5
Mohammed Refat <mohammedrefat53@outlook.com>
2023-12-04 19:07:18 +0200

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,10 @@ contains MyList class
 class MyList(list):
     """a subclass of list"""
 
+    def __init__(self):
+        """initializer"""
+        super().__init__()
+
     def print_sorted(self):
         """prints sorted list in ascending order"""
         print(sorted(self))
