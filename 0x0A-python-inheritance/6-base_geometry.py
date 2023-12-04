a21ec473cd4e15999b4bb461ec315d7b9fed1e0e
Mohammed Refat <mohammedrefat53@outlook.com>
2023-12-04 19:27:55 +0200

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """raise Exception
+        """
+        raise Exception("area() is not implemented")
