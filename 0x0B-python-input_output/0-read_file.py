809d96ab98f7426047d4a154217fdb6d91907aea
Mohammed Refat <mohammedrefat53@outlook.com>
2023-12-08 21:18:05 +0200

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""read_file function
+"""
+
+
+def read_file(filename=""):
+    """print to stdout the content of a file
+    Args:
+        filename
+    """
+    with open(filename, 'r') as f:
+        print(f)
