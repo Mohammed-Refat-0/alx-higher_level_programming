5c077685dce3828bb49fea8c91b4cdb3b8076614
Mohammed Refat <mohammedrefat53@outlook.com>
2023-12-08 22:50:25 +0200

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""load_from_json_file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a “JSON file”
+
+    Args:
+        filename (_type_): json file
+    """
+    with open(filename, 'r') as f:
+        obj = json.load(f)
