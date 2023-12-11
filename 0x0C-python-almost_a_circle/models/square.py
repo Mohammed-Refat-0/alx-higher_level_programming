7290618feed484716a6a3fd83c204c6b37ecc2b3
Mohammed Refat <mohammedrefat53@outlook.com>
2023-12-11 21:28:40 +0200

    10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,6 +26,10 @@ class Sqaure(Rectangle):
         """
             setter for size
         """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
