d66f45e22cfdd55ab1fe9aab8b3ac2e4ac821409
Mohammed Refat <mohammedrefat53@outlook.com>
2024-03-12 20:56:30 +0200

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// function that returns the reversed version of a list
+// Function that returns the reversed version of a list
 exports.esrever = function (list) {
   const x = list.length;
 
