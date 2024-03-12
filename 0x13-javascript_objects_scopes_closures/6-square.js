b9da61c83e65e06740966e38aa14a19c4ec104f8
Mohammed Refat <mohammedrefat53@outlook.com>
2024-03-12 20:39:45 +0200

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// Defines a "square" class that inherits from class "Rectangle"
+
+const Squarev = require('./5-square');
+
+class Square extends Squarev {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (isNaN(this.char)) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      console.log(c.repeat(this.size));
+    }
+  }
+}
+
+module.exports = Square;
