e61c6a0c1c731f48f52f5510abc04fa8b7e45fa2
Mohammed Refat <mohammedrefat53@outlook.com>
2024-03-17 00:04:14 +0200

    task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that takes in the name of a state as an argument
+and lists all cities of that state,
+using the database hbtn_0e_4_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    curs = db.cursor()
+    input = sys.argv[4]
+    curs.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                 WHERE states.name = %s""", (input, ))
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    db.close()
