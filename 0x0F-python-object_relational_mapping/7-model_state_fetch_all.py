5bd861f8957279b775ab15ca1817b7748c10132c
Mohammed Refat <mohammedrefat53@outlook.com>
2024-03-17 23:56:59 +0200

    task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,7 +15,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    results = session.query(State).all().order_by(State.id)
-
-    for i in results:
+    for instance in session.query(State).order_by(State.id):
         print(instance.id, instance.name, sep=": ")
