82a6de3e76c2c92a4cce720aa559eb51f196f015
Mohammed Refat <mohammedrefat53@outlook.com>
2024-03-17 22:54:56 +0200

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,9 +11,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-
-engine = create_engine(
-    'mysql+pymysql://username:password@localhost:3306/database_name')
-
-Base.metadata.create_all(engine)
