8c0f898d17e06990ea1eba7ba38d5b099d28cd09
Mohammed Refat <mohammedrefat53@outlook.com>
2024-03-20 21:28:44 +0200

    users_fix

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -15,6 +15,7 @@ class State(Base):
     Class that represents a state in the database.
     """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, unique=True,
+                autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
