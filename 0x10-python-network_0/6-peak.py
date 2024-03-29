5b64106a2cb8cc02f8db2733853be204f782333e
Mohammed Refat <mohammedrefat53@outlook.com>
2024-03-29 05:06:05 +0200

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,7 +4,7 @@
 
 def find_peak(list_of_integers):
     """Find a peak in a list of unsorted integers."""
-    if list_of_integers is None or list_of_integers == []:
+    if list_of_integers is None or not list_of_integers:
         return None
     list_of_integers.sort(reverse=True)
     return list_of_integers[0]
