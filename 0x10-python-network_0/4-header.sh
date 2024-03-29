01f5978aeeacd87f3e5fad3bed2eb4416629880f
Mohammed Refat <mohammedrefat53@outlook.com>
2024-03-29 04:06:29 +0200

    task 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to a given URL with a  custome header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+curl -sH -X GET "X-HolbertonSchool-User-Id: 98" "${1}"
